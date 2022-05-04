916e2a4c30c59fb9c5c6c7c7283b327fc4eef0f0
Ryan Selesnik <selesnikryan@gmail.com>
2022-05-04 16:34:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from mpi4py import MPI
+import pandas as pd
+import numpy as np
+Comm = MPI.COMM_WORLD
+my_rank = Comm.Get_rank()
+p = Comm.Get_size()
+
+name = MPI.Get_processor_name()
+global_means = []
+sorted_mags = []
+if my_rank == 0:
+    df = pd.read_csv('Accelerometer.csv')
+    split_dfs = np.array_split(df, p-1)
+
+    for i in range(0, p-1):
+        Comm.send(split_dfs[i], dest=i+1)
+
+
+if my_rank != 0:
+    my_df = Comm.recv(source=0)
+    vectors = my_df[['x', 'y', 'z']]
+    magnitudes = np.apply_along_axis(np.linalg.norm, 1, vectors)
+    stats = {}
+    stats['mean'] = magnitudes.mean()
+
+    magnitudes.sort()
+    stats['sorted_mags'] = magnitudes
+
+    Comm.send(stats, dest=0)
+else:
+    for procid in range(1, p):
+        stats = Comm.recv(source=procid)
+        global_means.append(stats['mean'])
+
+        np.concatenate(np.array(sorted_mags), np.array(stats['sorted_mags']))
+
+    global_means = np.array(global_means).mean()
+    print(global_means)
+    sorted_mags.sort()
+    print(sorted_mags)
