96ecc1dd7989c1141b77d81737389384193608a0
Ryan Selesnik <selesnikryan@gmail.com>
2022-05-09 16:39:07 +0200

    cleaned uo

--- parallelFileRead.py
+++
@@ -1,7 +0,0 @@
-from mpi4py import MPI
-import numpy as np
-
-comm = MPI.COMM_WORLD
-
-
-MPI.File.Get_size('./Accelerometer.csv')
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mpiexec -hostfile hostfile -n $1 python3 -m mpi4py main.py $2
+mpiexec -hostfile hostfile -n $1 python3 -m mpi4py src/main.py $2
