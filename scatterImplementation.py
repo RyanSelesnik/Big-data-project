d2bf5482d33bece189694417f6020ca393bdfb4e
Ryan Selesnik <selesnikryan@gmail.com>
2022-05-09 16:04:22 +0200

    new scatter file

--- scatterImplementation.py
+++ scatterImplementation.py
@@ -2,25 +2,36 @@ from fileinput import filename
 from mpi4py import MPI
 import pandas as pd
 import numpy as np
-from helperFunctions import getQuartile
+import sys
+import os
+
+from helperFunctions import getQuartile, preprocess_data
 from helperFunctions import get_epochs
 from helperFunctions import truncate_df_with_interval
 from helperFunctions import write_to_csv
-
-import sys
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 numprocs = comm.Get_size()
 
 if rank == 0:
+    answer = input(
+        'Run statistical analysis on entire file (1) or for a given time range (2): ')
     global_start_time = MPI.Wtime()
     filename = sys.argv[1]
     df = pd.read_csv(filename)
+    if answer == '2':
+        start_date = str(input("Enter start time (yyyy-mm-dd hh:mm:ss): "))
+        end_date = str(input("Enter end time (yyyy-mm-dd hh:mm:ss): "))
+        epoch_start, epoch_end = get_epochs(start_date, end_date)
+        truncate_df_with_interval(df, epoch_start, epoch_end)
+    if df.empty:
+        raise ValueError('Range does not exist in specified dataset')
+    preprocess_data(df)
     chunks = np.array_split(df, numprocs)
 else:
     chunks = None
-    
+
 chunk = comm.scatter(chunks, root=0)
 # Change to last three columns
 vectors = chunk[['x', 'y', 'z']]
@@ -74,9 +85,12 @@ if rank == 0:
         'upper_fence': upper_fence
     }
     total_time = MPI.Wtime() - global_start_time
-    write_to_csv(statistical_indicators)
+    output_file_name = 'statistical_indicators.csv'
+    write_to_csv(statistical_indicators, output_file_name)
 
+    print(f'\n\n---- Statistcal indicators for {filename} ----\n\n')
     print(
-        f'----------MPI-----------\n\n Number of processes: {numprocs}\nMedian:\t{median} \nQ1:\t{Q1} \nQ3: \t{Q3} \nIQR: \t{IQR}\n Minimum: \t{minimum}\n Maximum: \t{maximum} \nUpper fence: \t{upper_fence}\nLower fence: \t{lower_fence}'
+        f'Number of processes used: {numprocs}\n\nMedian:\t{median} \nQ1:\t{Q1} \nQ3: \t{Q3} \nIQR: \t{IQR}\nMinimum: \t{minimum}\nMaximum: \t{maximum} \nUpper fence: \t{upper_fence}\nLower fence: \t{lower_fence}'
     )
-    print(f'Final time is {total_time}')
+    print(f'\nThe above data has been written to ./{output_file_name}')
+    print(f'\nThe total execution time is {total_time}')
